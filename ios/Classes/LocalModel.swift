0c247471a7bea6843644cc3b9a5b1122628cd608
joonyeonglim <joon09@evidnet.co.kr>
2025-03-06 08:48:36 +0900

    compile mlmodelc

--- ios/Classes/LocalModel.swift
+++ ios/Classes/LocalModel.swift
@@ -1,30 +1,40 @@
 import CoreML
 
-public class LocalModel: YoloModel {
-  public var task: String
-  var modelPath: String
+public class LocalModel {
+    public var task: String
+    var modelPath: String
+    var model: MLModel?
 
-  public init(modelPath: String, task: String) {
-    self.modelPath = modelPath
-    self.task = task
-  }
-
-  public func loadModel() async throws -> MLModel? {
-    let fileURL = URL(fileURLWithPath: modelPath)
-    let fileExtension = fileURL.pathExtension.lowercased()
-    
-    do {
-      if fileExtension == "mlpackage" {
-        // mlpackage 형식은 직접 로드
-        return try MLModel(contentsOf: fileURL)
-      } else {
-        // mlmodel 형식은 컴파일 후 로드
-        let compiledModelURL = try await MLModel.compileModel(at: fileURL)
-        return try MLModel(contentsOf: compiledModelURL)
-      }
-    } catch {
-      print("Model loading error: \(error)")
-      throw error
+    public init(modelPath: String, task: String) {
+        self.modelPath = modelPath
+        self.task = task
     }
-  }
+
+    public func loadModel() throws {
+        let fileURL = URL(fileURLWithPath: modelPath)
+        let fileExtension = fileURL.pathExtension.lowercased()
+
+        do {
+            if fileExtension == "mlmodelc" {
+                // mlmodelc 형식은 직접 로드
+                self.model = try MLModel(contentsOf: fileURL)
+            } else if fileExtension == "mlmodel" {
+                // mlmodel 형식은 컴파일 후 로드
+                let compiledModelURL = try MLModel.compileModel(at: fileURL)
+                self.model = try MLModel(contentsOf: compiledModelURL)
+            } else {
+                throw NSError(domain: "Unsupported model file extension", code: -1, userInfo: nil)
+            }
+        } catch {
+            print("Model loading error: \(error)")
+            throw error
+        }
+    }
+
+    public func predict(input: MLFeatureProvider) throws -> MLFeatureProvider? {
+        guard let model = self.model else {
+            throw NSError(domain: "Model not loaded", code: -1, userInfo: nil)
+        }
+        return try model.prediction(from: input)
+    }
 }
