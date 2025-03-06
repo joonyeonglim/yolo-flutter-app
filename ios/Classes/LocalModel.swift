d6ea7148538191ac8fc081c0bcccaf5c1e4ca89b
joonyeong lim <joonyeonglim@Mac-mini.local>
2025-03-06 20:26:07 +0900

    fixed mlmodel

--- ios/Classes/LocalModel.swift
+++ ios/Classes/LocalModel.swift
@@ -2,11 +2,8 @@ import CoreML
 
 // YoloModel 프로토콜을 준수하도록 변경
 public class LocalModel: YoloModel {
-    public typealias MLModel = CoreML.MLModel
-    
-    public var task: String
-    var modelPath: String
-    private var model: MLModel?
+  public var task: String
+  var modelPath: String
 
     public init(modelPath: String, task: String) {
         self.modelPath = modelPath
@@ -20,15 +17,14 @@ public class LocalModel: YoloModel {
         do {
             if fileExtension == "mlmodelc" {
                 // 이미 컴파일된 모델은 직접 로드
-                model = try MLModel(contentsOf: fileURL)
+                return try MLModel(contentsOf: fileURL)
             } else if fileExtension == "mlmodel" || fileExtension == "mlpackage" {
                 // mlmodel과 mlpackage는 동일하게 컴파일 후 로드
                 let compiledModelURL = try MLModel.compileModel(at: fileURL)
-                model = try MLModel(contentsOf: compiledModelURL)
+                return try MLModel(contentsOf: compiledModelURL)
             } else {
                 throw NSError(domain: "Unsupported model file extension", code: -1, userInfo: nil)
             }
-            return model
         } catch {
             print("Model loading error: \(error)")
             throw error
