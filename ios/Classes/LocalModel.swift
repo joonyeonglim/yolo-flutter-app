f2fab4c116432e8499e59b89fdbd93631604a560
joonyeong lim <joonyeonglim@Mac-mini.local>
2025-03-06 19:42:28 +0900

    update

--- ios/Classes/LocalModel.swift
+++ ios/Classes/LocalModel.swift
@@ -1,40 +1,37 @@
 import CoreML
 
-public class LocalModel {
+// YoloModel 프로토콜을 준수하도록 변경
+public class LocalModel: YoloModel {
+    public typealias MLModel = CoreML.MLModel
+    
     public var task: String
     var modelPath: String
-    var model: MLModel?
+    private var model: MLModel?
 
     public init(modelPath: String, task: String) {
         self.modelPath = modelPath
         self.task = task
     }
 
-    public func loadModel() throws {
+    public func loadModel() async throws -> MLModel? {
         let fileURL = URL(fileURLWithPath: modelPath)
         let fileExtension = fileURL.pathExtension.lowercased()
 
         do {
             if fileExtension == "mlmodelc" {
                 // 이미 컴파일된 모델은 직접 로드
-                self.model = try MLModel(contentsOf: fileURL)
+                model = try MLModel(contentsOf: fileURL)
             } else if fileExtension == "mlmodel" || fileExtension == "mlpackage" {
                 // mlmodel과 mlpackage는 동일하게 컴파일 후 로드
                 let compiledModelURL = try MLModel.compileModel(at: fileURL)
-                self.model = try MLModel(contentsOf: compiledModelURL)
+                model = try MLModel(contentsOf: compiledModelURL)
             } else {
                 throw NSError(domain: "Unsupported model file extension", code: -1, userInfo: nil)
             }
+            return model
         } catch {
             print("Model loading error: \(error)")
             throw error
         }
-    }
-
-    public func predict(input: MLFeatureProvider) throws -> MLFeatureProvider? {
-        guard let model = self.model else {
-            throw NSError(domain: "Model not loaded", code: -1, userInfo: nil)
-        }
-        return try model.prediction(from: input)
     }
 }
